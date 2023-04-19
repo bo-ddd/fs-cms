eee1e6cfe3bee0cfdf11745edcb58affc40cba9e
yang <1145938287@qq.com>
2023-04-19 09:16:34 +0800

    car

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import "@/assets/lib/lib-flexiable.js"
 import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
+// import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 import 'element-plus/dist/index.css'
 
 import './assets/main.css'
